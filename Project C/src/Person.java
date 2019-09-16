0f8ea73cf1ea1bd12b343b800e3aaeabc9b9c23d
laurensmaas <0977587@hr.nl>
2019-09-16 14:58:27 +0200

    added travis
    travis added

--- Project C/src/Person.java
+++ Project C/src/Person.java
@@ -7,5 +7,36 @@ public class Person {
 	public Person()	 {
 		// TODO Auto-generated constructor stub
 	}
+	public Person(String name, int age, String adres, String gender) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.adres = adres;
+		this.gender = gender;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAdres() {
+		return adres;
+	}
+	public void setAdres(String adres) {
+		this.adres = adres;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
 }
