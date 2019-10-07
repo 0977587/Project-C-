4db5f06a3c824479a1f1eaab5aca491c17d3bef6
unknown <laurensmaas9702@gmail.com>
2019-10-07 15:54:06 +0200

    different file
    try this

---
+++ RazorPagesQuestions/RazorPagesQuestion/RazorPagesQuestion/Models/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesQuestions.Models
+{
+    public class Question
+    {
+        
+        public string ID { get; set; }
+
+        
+        public string Naam { get; set; }
+
+        public string Vraag { get; set; }
+        public string Vak { get; set; }
+        public string Lokaal { get; set; }
+    }
+}
---
+++ RazorPagesQuestions/RazorPagesQuestion/RazorPagesQuestion/Pages/Questions/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesQuestion.Data;
+using RazorPagesQuestions.Models;
+
+namespace RazorPagesQuestion.Pages.Questions
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesQuestion.Data.RazorPagesQuestionContext _context;
+
+        public CreateModel(RazorPagesQuestion.Data.RazorPagesQuestionContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Question Question { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+           
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _context.Question.Add(Question);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
