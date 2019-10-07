ab3090de406c2e51417e609bf0954123ee3eee85
unknown <laurensmaas9702@gmail.com>
2019-10-07 10:11:48 +0200

    smaal update
    added create.cshtml

---
+++ RazorPagesQuestions/Questions/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesQuestions.Data;
+using RazorPagesQuestions.Models;
+
+namespace RazorPagesQuestions.Pages.Questions
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesQuestions.Data.RazorPagesQuestionsContext _context;
+
+        public CreateModel(RazorPagesQuestions.Data.RazorPagesQuestionsContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Question Question { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Question.Add(Question);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
