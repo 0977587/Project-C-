71b99f8a6a5c4cba2c43534ca3aaf023c546559a
unknown <laurensmaas9702@gmail.com>
2019-10-07 10:10:56 +0200

    big update with new functionality
    1. added database
    2. added stel een vraag
    3. added overzicht vragen

--- Project C/src/text
+++
@@ -1,7 +0,0 @@
-require 'sinatra'
-require 'json'
-
-post '/event_handler' do
-  payload = JSON.parse(params[:payload])
-  "Well, it worked!"
-end
---
+++ RazorPagesQuestions/Models/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesQuestions.Models
+{
+    public class Question
+    {
+        public int ID { get; set; }
+        public string Naam { get; set; }
+
+        public string Vraag { get; set; }
+        public string Vak { get; set; }
+        public string Lokaal { get; set; }
+    }
+}
