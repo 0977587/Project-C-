b2a0c364317c36523a30e5aa35424d5d56625cda
unknown <laurensmaas9702@gmail.com>
2019-10-16 15:28:49 +0200

    added stel een vraag
    Added stel een vraag

---
+++ webapp/webapp/Data/webappContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using webapp.Models;
+
+namespace webapp.Data
+{
+    public class webappContext : DbContext
+    {
+        public webappContext (DbContextOptions<webappContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<webapp.Models.Question> Question { get; set; }
+    }
+}
---
+++ webapp/webapp/Pages/Vragen/Steleenvraag/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using webapp.Data;
+using webapp.Models;
+
+namespace webapp.Pages.Vragen.Steleenvraag
+{
+    public class CreateModel : PageModel
+    {
+        private readonly webapp.Data.webappContext _context;
+
+        public CreateModel(webapp.Data.webappContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Question Question { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Question.Add(Question);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
