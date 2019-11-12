a1e58783afcddbba1b316b094fc1e0de5b5a1832
Marnick76 <marnickdezwart@gmail.com>
2019-11-12 12:51:26 +0100

    .
    .

---
+++ loginaspnetcore/loginaspnetcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace loginaspnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
