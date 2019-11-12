189020accaa3593d83e3b07f2c61bb280555282a
unknown <0977587@hr.nl>
2019-11-12 15:42:33 +0100

    added functionality stel een vraag
    added stel een vraag to the database
    vraagID is always initiated with 0 //TODO

--- webapp/webapp/Models/Queue.cs
+++ webapp/webapp/Models/Queue.cs
@@ -8,10 +8,9 @@ namespace webapp.Models
     public class Queue
     {
         public int WachtrijID { get; set; }
-        public List<string> Peercoaches { get; set; }
-        public List<Vraag> Vragen { get; set; }
         public DateTime DateAdded { get; set; }
         public DateTime EndDate { get; set; }
+        public String Name { get; set; }
         //DateAdded wanneer aangemaatk DB>.Send("get * where db.vraag.DateAdded later dan DateAdded"
     }
 }
--- webapp/webapp/Pages/Vragen/Steleenvraag/Create.cshtml.cs
+++ webapp/webapp/Pages/Vragen/Steleenvraag/Create.cshtml.cs
@@ -25,31 +25,23 @@ namespace webapp.Pages.Vragen.Steleenvraag
 
         public void OnGet()
         {
-            VraagText = "voer hier je vraag in";
+            //VraagText = "voer hier je vraag in";
 
         }
 
         public void OnPost()
         {
-
-
-            VraagID = 1;
-            UserID = 1;
-
-
-            List<List<string>> connect = new DBConnection().Send("");
-            
-
+            VraagID = 0;
+            UserID = Sessie.GetInstance.getLoginUserID();
             VraagText = Request.Form[nameof(VraagText)];
 
+            
+            
             AndwoordText = "";
             DateAdded = DateTime.UtcNow;
             EndDate = DateTime.MinValue;
-
-
-            Vraag temp = new Vraag(VraagID, UserID, 1, VraagText, AndwoordText,false, DateAdded, EndDate);
+            Vraag temp = new Vraag(0, UserID, 1, VraagText, AndwoordText,false, DateAdded, EndDate);
             temp.Insert();
-            
         }
     }
 }
