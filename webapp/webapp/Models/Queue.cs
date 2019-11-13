bd4aa37b4ce7ce6f2d050b2b1bf9e40a3e0290b3
Matthijs <matthijshofele@gmail.com>
2019-11-13 11:27:24 +0100

    Nieuwste versie, runt niet

--- webapp/webapp/Models/Queue.cs
+++ webapp/webapp/Models/Queue.cs
@@ -11,6 +11,58 @@ namespace webapp.Models
         public DateTime DateAdded { get; set; }
         public DateTime EndDate { get; set; }
         public String Name { get; set; }
-        //DateAdded wanneer aangemaatk DB>.Send("get * where db.vraag.DateAdded later dan DateAdded"
+
+        public Wachtrij(int wachtrijID, DateTime dateAdded, DateTime endDate, string name)
+        {
+            WachtrijID = wachtrijID;
+            DateAdded = dateAdded;
+            EndDate = endDate;
+            Name = name;
+        }
+
+        // lege constructor 
+
+        public Wachtrij()
+        {
+
+        }
+
+        // constructor zonder naam
+        public Wachtrij(int wachtrijID, DateTime dateAdded, DateTime endDate)
+        {
+            WachtrijID = wachtrijID;
+            DateAdded = dateAdded;
+            EndDate = endDate;
+            Name = null;
+        }
+
+        public void SelectOne(int input)
+        {
+            // geef WachtrijID mee
+            List<List<string>> returnstatement = new DBConnection().Send("SELECT * FROM projectcdb.user WHERE UserID = " + input + ";");
+            WachtrijID = Convert.ToInt32(returnstatement[0][0]);
+            if (returnstatement[0][1] != "")
+                DateAdded = Convert.ToDateTime(returnstatement[0][1]);
+            if (returnstatement[0][2] != "")
+                EndDate = Convert.ToDateTime(returnstatement[0][2]);
+            if (returnstatement[0][3] != "")
+                Name = returnstatement[0][3];
+        }
+
+        public void Delete()
+        {
+            //geef WachtrijID mee  
+            List<List<string>> returnstatement = new DBConnection().Send("DELETE FROM `projectcdb`.`Wachtrij` WHERE (`WachtrijID` = '" + WachtrijID + "');");
+        }
+
+        public void Update()
+        {
+            new DBConnection().Send("UPDATE `projectcdb`.`Wachtrij` SET `WachtrijID` = '" + WachtrijID + "', `DateAdded` = '" + DateAdded + "', `EndDate` = '" + EndDate + "', `Name` = '" + Name + "');");
+        }
+
+        public void Insert()
+        {
+            new DBConnection().Send("UPDATE `projectcdb`.`Wachtrij` SET `WachtrijID` = '" + WachtrijID + "', `DateAdded` = '" + DateAdded + "', `EndDate` = '" + EndDate + "', `Name` = '" + Name + "');");
+        }
     }
 }
