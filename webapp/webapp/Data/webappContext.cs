fb6801528e4c6a0ca6b34b075adf3dc5cea7e50d
Matthijs <matthijshofele@gmail.com>
2019-12-02 12:28:44 +0100

    Update

---
+++ webapp/webapp/Data/webappContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using webapp.Models;
+
+namespace webapp.Data
+{
+    public class WebappContext : DbContext
+    {
+        public WebappContext (DbContextOptions<WebappContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<webapp.Models.Vraag> Vraag { get; set; }
+    }
+}
---
+++ webapp/webapp/Pages/Registratie/PeerRegistratie.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using DatabaseController;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using webapp.Models;
+
+
+
+namespace webapp.Pages.Registratie
+{
+    public class PeerRegistratiemodel : PageModel
+    {
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Email { get; set; }
+        public string Wachtwoord { get; set; }
+        public string peercoach { get; set; }
+        public string PeercoachW { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            string p = Request.Form[nameof(peercoach)];
+            if(Convert.ToInt32(p) != 1)
+            {
+
+            }
+            else
+            {
+                Response.Redirect("./PeerRegistratie");
+            }
+        }
+    }
+}
