17553b24ef442d734b36c022a66cbfa10812ac11
unknown <0977587@hr.nl>
2019-12-02 13:58:09 +0100

    added branch matthijs
    matthijs branchh update

---
+++ webapp/Pages/Menu/PMenu.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Pages
+{
+    public class PMenuModel : PageModel
+    {
+        public static void OnGet()
+        {
+
+        }
+
+
+
+        public void OnPost()
+        {
+
+            Sessie.GetInstance.setLoginUserID(-1);
+
+            Response.Redirect("../Index");
+        }
+    }
+}
