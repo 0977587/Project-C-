1a0178ba729fb4c7812a13f1b5d61314b1e32380
unknown <0977587@hr.nl>
2019-12-09 14:21:57 +0100

    added matthisj tests to my tests
    added a lot of tests to my tests

---
+++ webapp/UnitTest/Models/IngeschrevenVakkenTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using webapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webapp.Models.Tests
+{
+    [TestClass()]
+    public class IngeschrevenVakkenTests
+    {
+        Vak v = new Vak(1, "x", "x", "x", "x", false);
+        User u = new User("s", 1, "x", "x", "x", "x");
+
+
+
+        [TestMethod()]
+        public void IngeschrevenVakkenTest()
+        {
+            IngeschrevenVakken i = new IngeschrevenVakken(v, u);
+            if (i.VakID != v || i.UserID != u)
+            {
+                Assert.Fail();
+            }
+
+            
+        }
+
+        [TestMethod()]
+        public void TotaalingeschrevenVakkenTest()
+        {
+            IngeschrevenVakken i = new IngeschrevenVakken(v, u);
+            IngeschrevenVakken i2 = new IngeschrevenVakken(v, u);
+            IngeschrevenVakken i3 = new IngeschrevenVakken(v, u);
+
+            List<IngeschrevenVakken> vakken = new List<IngeschrevenVakken>();
+
+            vakken.Add(i);
+            vakken.Add(i2);
+            vakken.Add(i3);
+
+            TotaalingeschrevenVakken tot = new TotaalingeschrevenVakken(vakken);
+
+            if(tot.Vakken[1] != i2)
+            {
+                Assert.Fail();
+            }
+
+        }
+    }
+}
